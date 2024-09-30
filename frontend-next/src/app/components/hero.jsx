233f8ff47d34b36e18701c83d2b0d624fa35a5c4
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-09-30 22:27:33 +0700

    moved posts to public folder, added coverimg

--- frontend-next/src/app/components/hero.jsx
+++ frontend-next/src/app/components/hero.jsx
@@ -1,13 +1,13 @@
 const Hero = () => {
     return (
-      <div class="hero bg-base-100 min-h-screen">
-        <div class="hero-content text-center bg-base-100">
-          <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Hello there</h1>
-            <p class="py-6 text-accent-content">
+      <div className="hero bg-base-100 min-h-screen">
+        <div className="hero-content text-center bg-base-100">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6 text-accent-content">
               Welcome to my personal blogs, where I share useful tips on English learning.
             </p>
-            <button class="btn btn-primary">Get Started</button>
+            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
--- frontend-next/src/lib/mdx/index.jsx
+++ frontend-next/src/lib/mdx/index.jsx
@@ -2,16 +2,30 @@ import fs from 'fs'
 import path from 'path'
 import { compileMDX } from 'next-mdx-remote/rsc'
 import Slider from '@/app/components/Slider'
+import Image from 'next/image'
 
-const rootDirectory = path.join(process.cwd(),'src', 'posts')
+const rootDirectory = path.join(process.cwd(),'public', 'posts')
 
 const components = {
-    Slider
+    Slider,
+    img: (props) => {
+      const { src, alt, ...rest } = props
+      const fullSrc = src.startsWith('/') ? src : `/posts/${realSlug}/${src}`
+      return (
+        <Image
+          src={fullSrc}
+          alt={alt}
+          width={600}
+          height={400}
+          {...rest}
+        />
+      )
+    }
 }
 
 export const getPostBySlug = async slug => {
   const realSlug = slug.replace(/\.mdx$/, '')
-  const filePath = path.join(rootDirectory, `${realSlug}.mdx`)
+  const filePath = path.join(rootDirectory,`${realSlug}`, `index.mdx`)
 
   const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
 
@@ -20,8 +34,11 @@ export const getPostBySlug = async slug => {
     options: { parseFrontmatter: true },
     components: components
   })
+  const coverImg = frontmatter.coverImg 
+    ? `/posts/${realSlug}/${frontmatter.coverImg}`
+    : null
 
-  return { meta: { ...frontmatter, slug: realSlug }, content }
+  return { meta: { ...frontmatter, slug: realSlug, coverImg: coverImg }, content }
 }
 
 export const getAllPostsMeta = async () => {
