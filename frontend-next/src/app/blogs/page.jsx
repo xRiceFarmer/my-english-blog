995cd1c8a8a673d8efa848ebf03cd2fc354ce098
xRiceFarmer <khangthaidonguyen@gmail.com>
2024-10-01 09:33:35 +0700

    changed logs layout

--- frontend-next/src/app/blogs/page.jsx
+++ frontend-next/src/app/blogs/page.jsx
@@ -1,42 +1,19 @@
 import Link from "next/link";
-import Image from 'next/image'
+import Image from "next/image";
+import BlogCard from "../components/blog-card";
 import { getAllPostsMeta } from "@/lib/mdx";
 
 export default async function Page() {
   const posts = await getAllPostsMeta();
   return (
     <section className="py-24 bg-base-100">
-      <div className="container">
-        <h1 className="text-3xl text-base-content font-bold">All Posts</h1>
-        {posts?.map((post) => (
-          <div
-            key={post?.title}
-            className="card card-compact bg-base-100 w-96 shadow-xl "
-          >
-            <Link
-              href={`blogs/${post.slug}`}
-              className="p-8 rounded-md shadow-md"
-            >
-              <figure>
-                <Image
-                  src={post.coverImg} 
-                  alt={post.title}
-                  width={600}
-                  height={400}
-                />
-              </figure>
-              <div className="card-body text-base-content">
-                <h2 className="card-title text-base-content">
-                  {post.title}
-                  <time className="badge badge-secondary">
-                    {post.publishDate}
-                  </time>
-                </h2>
-                <p className="mt-4 text-sm text-base-content">{post.author}</p>
-              </div>
-            </Link>
-          </div>
-        ))}
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl text-base-content font-bold mb-8">All Posts</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          {posts?.map((post) => (
+            <BlogCard post={post} key={post.slug} />
+          ))}
+        </div>
       </div>
     </section>
   );
